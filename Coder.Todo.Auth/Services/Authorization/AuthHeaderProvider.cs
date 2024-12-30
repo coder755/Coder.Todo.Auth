6fbf369b73dd65f7b31140abd2ff990a1e566fea
coder755 <coderjamespark@gmail.com>
2024-12-30 11:58:02 -0700

    Clean up Auth Header Provider

--- Coder.Todo.Auth/Services/Authorization/AuthHeaderProvider.cs
+++ Coder.Todo.Auth/Services/Authorization/AuthHeaderProvider.cs
@@ -11,11 +11,13 @@ public class AuthHeaderProvider(
     public Guid GetUserIdFromToken()
     {
         var authHeader = httpContextAccessor.HttpContext?.Request?.Headers.Authorization.FirstOrDefault();
-        if (authHeader != null && authHeader.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase)) {
-            var token = authHeader.Substring("Bearer ".Length).Trim();
-            var sub = jwtService.GetSubjectFromToken(token);
-            return sub != null ? Guid.Parse(sub) : Guid.Empty;
+        if (authHeader == null || !authHeader.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase))
+        {
+            return Guid.Empty;
         }
-        return Guid.Empty;
+        
+        var token = authHeader.Substring("Bearer ".Length).Trim();
+        var sub = jwtService.GetSubjectFromToken(token);
+        return sub == null ? Guid.Empty : Guid.Parse(sub);
     }
 }
